31fb2f87d8844fc0874ad2df105e6eb58de9eabf
natmess <nnmessier@gmail.com>
2018-10-30 18:35:31 -0400

    Updated Calculator

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -8,17 +8,44 @@
 
 import Foundation
 
-func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
-  case "+":
-    return {x, y in x + y }
-  case "-":
-    return {x, y in x - y }
-  case "*":
-    return {x, y in x * y }
-  case "/":
-    return {x, y in x / y }
-  default:
-    return {x, y in x + y }
-  }
+var calculatorStatus = true
+
+
+if let userInput = readLine() {
+    
 }
+
+
+
+let components = userInput.components(separatedBy: " ")
+print(components)
+
+func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
+    
+    switch opString {
+    case "+":
+        return {x, y in x + y }
+    case "-":
+        return {x, y in x - y }
+    case "*":
+        return {x, y in x * y }
+    case "/":
+        return {x, y in x / y }
+    default:
+        return {x, y in x + y }
+    }
+}
+var mathOperation = mathStuffFactory(opString: "/")
+
+
+//calculate result using closure returned from mathStuffFactory
+var firstNum = Double()
+var secondNum = Double()
+if let firstNumber = Double(components[0]) {
+    firstNum = firstNumber
+}
+if let secondNumber = Double(components[2]) {
+    secondNum = secondNumber
+}
+let result = mathOperation(firstNum,secondNum)
+print("\(userInput) = \(result)")
